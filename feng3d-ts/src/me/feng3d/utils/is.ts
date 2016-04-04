da6bfc377ba570c7560dbcbbb002372a80142ef8
feng <908087098@qq.com>
2016-04-04 16:45:57 +0800

    添加 is as 方法

--- feng3d-ts/src/me/feng3d/utils/is.ts
+++ feng3d-ts/src/me/feng3d/utils/is.ts
@@ -1,7 +1,9 @@
 module feng3d {
 
+    /**
+     * 判断a对象是否为b类型
+     */
     export function is(a, b: Function): boolean {
-        var aClassName = getQualifiedClassName(a);
 
         var prototype: any = a.prototype ? a.prototype : Object.getPrototypeOf(a);
         while (prototype != null) {
@@ -9,7 +11,7 @@ module feng3d {
             if (prototype.constructor == b)
                 return true;
             //父类就是原型的原型构造函数
-            prototype = Object.getPrototypeOf(a);
+            prototype = Object.getPrototypeOf(prototype);
         }
 
         return false;
