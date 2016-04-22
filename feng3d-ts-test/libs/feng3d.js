696e4e31380d6fec9d3699e8e042d74a81eb0318
feng <908087098@qq.com>
2016-04-23 01:09:20 +0800

    制作 事件机制

--- feng3d-ts-test/libs/feng3d.js
+++ feng3d-ts-test/libs/feng3d.js
@@ -6,6 +6,26 @@ var __extends = (this && this.__extends) || function (d, b) {
 var feng3d;
 (function (feng3d) {
     /**
+     * 数学常量类
+     */
+    var MathConsts = (function () {
+        function MathConsts() {
+        }
+        /**
+         * 弧度转角度因子
+         */
+        MathConsts.RADIANS_TO_DEGREES = 180 / Math.PI;
+        /**
+         * 角度转弧度因子
+         */
+        MathConsts.DEGREES_TO_RADIANS = Math.PI / 180;
+        return MathConsts;
+    }());
+    feng3d.MathConsts = MathConsts;
+})(feng3d || (feng3d = {}));
+var feng3d;
+(function (feng3d) {
+    /**
      * 点与面的相对位置
      * @author feng
      */
@@ -43,26 +63,6 @@ var feng3d;
         return PlaneClassification;
     }());
     feng3d.PlaneClassification = PlaneClassification;
-})(feng3d || (feng3d = {}));
-var feng3d;
-(function (feng3d) {
-    /**
-     * 数学常量类
-     */
-    var MathConsts = (function () {
-        function MathConsts() {
-        }
-        /**
-         * 弧度转角度因子
-         */
-        MathConsts.RADIANS_TO_DEGREES = 180 / Math.PI;
-        /**
-         * 角度转弧度因子
-         */
-        MathConsts.DEGREES_TO_RADIANS = Math.PI / 180;
-        return MathConsts;
-    }());
-    feng3d.MathConsts = MathConsts;
 })(feng3d || (feng3d = {}));
 var feng3d;
 (function (feng3d) {
@@ -2075,8 +2075,8 @@ var feng3d;
             if (bubbles === void 0) { bubbles = false; }
             if (cancelable === void 0) { cancelable = false; }
             this._type = type;
-            this._data = data;
             this._bubbles = bubbles;
+            this.data = data;
         }
         /**
          * 防止对事件流中当前节点中和所有后续节点中的事件侦听器进行处理。此方法会立即生效，并且会影响当前节点中的事件侦听器。相比之下，在当前节点中的所有事件侦听器都完成处理之前，stopPropagation() 方法不会生效。
@@ -2113,26 +2113,18 @@ var feng3d;
             enumerable: true,
             configurable: true
         });
-        Object.defineProperty(Event.prototype, "data", {
-            /** 事件携带的自定义数据 */
-            get: function () {
-                return this._data;
-            },
-            /**
-             * @private
-             */
-            set: function (value) {
-                this._data = value;
-            },
-            enumerable: true,
-            configurable: true
-        });
         Object.defineProperty(Event.prototype, "target", {
             /**
              * 事件目标。
              */
             get: function () {
                 return this._target;
+            },
+            set: function (value) {
+                this._currentTarget = value;
+                if (this._target == null) {
+                    this._target = value;
+                }
             },
             enumerable: true,
             configurable: true
@@ -2161,12 +2153,6 @@ var feng3d;
             enumerable: true,
             configurable: true
         });
-        Event.prototype.setTarget = function (value) {
-            this._target = value;
-        };
-        Event.prototype.setCurrentTarget = function (value) {
-            this._currentTarget = value;
-        };
         return Event;
     }());
     feng3d.Event = Event;
@@ -2184,95 +2170,148 @@ var feng3d;
          */
         function EventDispatcher(target) {
             if (target === void 0) { target = null; }
+            /**
+             * 冒泡属性名称为“parent”
+             */
+            this.bubbleAttribute = "parent";
             this._target = target;
             if (this._target == null)
                 this._target = this;
-            this._eventListeners = {};
+            this.listenerVOUtils = new ListenerVOUtils();
         }
-        EventDispatcher.prototype.addEventListener = function (type, thisObject, listener, priority, useWeakReference) {
+        /**
+         * 使用 EventDispatcher 对象注册事件侦听器对象，以使侦听器能够接收事件通知。
+         * @param type						事件的类型。
+         * @param listener					处理事件的侦听器函数。此函数必须接受 Event 对象作为其唯一的参数，并且不能返回任何结果，如下面的实例所示： <pre>function(evt:Event):void</pre>函数可以有任何名称。
+         * @param thisObject                listener函数作用域
+         * @param priority					事件侦听器的优先级。优先级由一个带符号的 32 位整数指定。数字越大，优先级越高。优先级为 n 的所有侦听器会在优先级为 n -1 的侦听器之前得到处理。如果两个或更多个侦听器共享相同的优先级，则按照它们的添加顺序进行处理。默认优先级为 0。
+         */
+        EventDispatcher.prototype.addEventListener = function (type, listener, thisObject, priority) {
             if (priority === void 0) { priority = 0; }
-            if (useWeakReference === void 0) { useWeakReference = false; }
             if (listener == null)
                 return;
-            var listeners = this._eventListeners[type];
-            if (listeners == null)
-                listeners = this._eventListeners[type] = [];
-            var index = listeners.indexOf(listener);
-            if (index == -1) {
-                listeners.push(listener);
-            }
-        };
-        EventDispatcher.prototype.removeEventListener = function (type, listener) {
-            if (this._eventListeners) {
-                var listeners = this._eventListeners[type];
-                var index = listeners.indexOf(listener);
-                listeners.splice(index, 1);
-            }
-        };
-        EventDispatcher.prototype.removeEventListeners = function (type) {
-            if (type === void 0) { type = null; }
-            if (type && this._eventListeners)
-                delete this._eventListeners[type];
-            else
-                this._eventListeners = {};
+            var listeners = this.listenerVOUtils.getListeners(type);
+            this.listenerVOUtils.remove(listeners, listener, thisObject);
+            this.listenerVOUtils.add(listeners, listener, thisObject, priority);
         };
         /**
-         * @inheritDoc
+         * 从 EventDispatcher 对象中删除侦听器. 如果没有向 EventDispatcher 对象注册任何匹配的侦听器，则对此方法的调用没有任何效果。
+         *
+         * @param type						事件的类型。
+         * @param listener					要删除的侦听器对象。
+         * @param thisObject                listener函数作用域
+         */
+        EventDispatcher.prototype.removeEventListener = function (type, listener, thisObject) {
+            var listeners = this.listenerVOUtils.getListeners(type);
+            this.listenerVOUtils.remove(listeners, listener, thisObject);
+        };
+        /**
+         * 将事件调度到事件流中. 事件目标是对其调用 dispatchEvent() 方法的 EventDispatcher 对象。
+         *
+         * @param event						调度到事件流中的 Event 对象。如果正在重新调度事件，则会自动创建此事件的一个克隆。在调度了事件后，其 target 属性将无法更改，因此您必须创建此事件的一个新副本以能够重新调度。
+         * @return 							如果成功调度了事件，则值为 true。值 false 表示失败或对事件调用了 preventDefault()。
+         *
          */
         EventDispatcher.prototype.dispatchEvent = function (event) {
-            var _this = this;
             //停止事件流
             if (!event || event.stopsPropagation)
                 return false;
             //设置目标
-            if (!event.target)
-                event.setTarget(this._target);
+            event.target = this._target;
             //处理当前事件(目标阶段)
-            var listeners = this._eventListeners[event.type];
-            if (!event.stopsImmediatePropagation) {
-                listeners.forEach(function (listener) {
-                    //设置当前目标
-                    event.setCurrentTarget(_this._target);
-                    listener(event);
-                });
-            }
+            var listeners = this.listenerVOUtils.getListeners(event.type);
+            this.listenerVOUtils.dispatchEvent(listeners, event);
             //事件冒泡(冒泡阶段)
             if (event.bubbles && this.parentDispatcher) {
                 this.parentDispatcher.dispatchEvent(event);
             }
             return event.stopsPropagation;
         };
-        EventDispatcher.prototype.hasEventListener = function (type) {
-            var listeners = this._eventListeners ? this._eventListeners[type] : null;
-            for (var key in listeners) {
-                return true;
-            }
-            return false;
-        };
         /**
-         * 该功能暂未实现
-         * @param type
-         * @return
+         * 检查 EventDispatcher 对象是否为特定事件类型注册了任何侦听器. 这样，您就可以确定 EventDispatcher 对象在事件流层次结构中的哪个位置改变了对事件类型的处理。要确定特定事件类型是否确实触发了事件侦听器，请使用 willTrigger()。
+         *
+         * @param type		事件的类型。
+         * @return 			如果指定类型的侦听器已注册，则值为 true；否则，值为 false。
          */
-        EventDispatcher.prototype.willTrigger = function (type) {
-            // TODO Auto Generated method stub
-            return false;
+        EventDispatcher.prototype.hasEventListener = function (type) {
+            var listeners = this.listenerVOUtils.getListeners(type);
+            return listeners.length > 0;
         };
         Object.defineProperty(EventDispatcher.prototype, "parentDispatcher", {
             /**
              * 父事件适配器
              */
             get: function () {
-                return this._target[EventDispatcher.BUBBLE_PROPERTY];
+                return this._target[this.bubbleAttribute];
             },
             enumerable: true,
             configurable: true
         });
-        /** 冒泡属性名称为“parent” */
-        EventDispatcher.BUBBLE_PROPERTY = "parent";
         return EventDispatcher;
     }());
     feng3d.EventDispatcher = EventDispatcher;
+    /**
+     * 监听数据
+     */
+    var ListenerVO = (function () {
+        function ListenerVO() {
+        }
+        return ListenerVO;
+    }());
+    /**
+     * 监听数据工具类
+     */
+    var ListenerVOUtils = (function () {
+        function ListenerVOUtils() {
+            this.eventListeners = {};
+        }
+        /**
+         * 获取某类型事件的监听列表
+         */
+        ListenerVOUtils.prototype.getListeners = function (type) {
+            var listeners = this.eventListeners[type];
+            if (listeners == null)
+                listeners = this.eventListeners[type] = [];
+            return listeners;
+        };
+        /**
+         * 添加监听
+         */
+        ListenerVOUtils.prototype.add = function (listeners, listener, thisObject, priority) {
+            if (thisObject === void 0) { thisObject = null; }
+            if (priority === void 0) { priority = 0; }
+            var addItem = { listener: listener, thisObject: thisObject, priority: priority };
+            for (var i = 0; i < listeners.length; i++) {
+                var element = listeners[i];
+                if (addItem.priority > element.priority) {
+                    break;
+                }
+            }
+            listeners.splice(i, 0, addItem);
+        };
+        /**
+         * 移除监听
+         */
+        ListenerVOUtils.prototype.remove = function (listeners, listener, thisObject) {
+            if (thisObject === void 0) { thisObject = null; }
+            for (var i = listeners.length - 1; i >= 0; i--) {
+                var element = listeners[i];
+                if (element.listener == listener && element.thisObject == thisObject) {
+                    listeners.splice(i, 1);
+                }
+            }
+        };
+        /**
+         * 派发事件
+         */
+        ListenerVOUtils.prototype.dispatchEvent = function (listeners, event) {
+            for (var i = 0; i < listeners.length && !event.stopsImmediatePropagation; i++) {
+                var element = listeners[i];
+                element.listener.apply(element.thisObject, event);
+            }
+        };
+        return ListenerVOUtils;
+    }());
 })(feng3d || (feng3d = {}));
 var feng3d;
 (function (feng3d) {
