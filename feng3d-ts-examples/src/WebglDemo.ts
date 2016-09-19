1fddbbd119baea0454eb22b400e1d672913e1277
feng <908087098@qq.com>
2016-09-20 00:32:51 +0800

    整理 示例代码

--- feng3d-ts-examples/src/WebglDemo.ts
+++ feng3d-ts-examples/src/WebglDemo.ts
@@ -1,56 +1,58 @@
+module me.feng3d {
 
+    export class WebglDemo {
 
-function start() {
+        view3D: me.feng3d.View3D;
+        constructor() {
 
-    var canvas = document.getElementById("glcanvas");
-    view3D = new me.feng3d.View3D(canvas);
+            this.init();
+        }
 
-    var cube = me.feng3d.Object3D.createPrimitive(me.feng3d.PrimitiveType.Cube);
-    cube.space3D.x = -100;
-    cube.space3D.y = 100;
-    cube.space3D.z = 300;
-    cube.space3D.rx = 45;
-    cube.space3D.ry = 45;
-    view3D.scene.addObject3D(cube);
+        init() {
+            var canvas = document.getElementById("glcanvas");
+            this.view3D = new me.feng3d.View3D(canvas);
 
-    var plane0 = new me.feng3d.Object3D("p", [
-        me.feng3d.primitives.createPlane(),
-        new me.feng3d.Space3D(0, 0, 300, 90)
-    ]);
-    
-    view3D.scene.addObject3D(plane0);
+            // var cube = me.feng3d.Object3D.createPrimitive(me.feng3d.PrimitiveType.Cube);
+            // cube.space3D.x = -100;
+            // cube.space3D.y = 100;
+            // cube.space3D.z = 300;
+            // cube.space3D.rx = 45;
+            // cube.space3D.ry = 45;
+            // this.view3D.scene.addObject3D(cube);
 
-    var plane = new me.feng3d.Object3D("plane", [
-        me.feng3d.primitives.createPlane(),
-        new me.feng3d.Space3D(100, 100, 300, 90, 0, 45),
-    ]);
-    view3D.scene.addObject3D(plane);
+            var cube = new me.feng3d.Object3D("cube", [
+                me.feng3d.primitives.createCube(),
+                new me.feng3d.Space3D(-100, 100, 300, 45, 45.0),
+            ]);
+            this.view3D.scene.addObject3D(cube);
 
-    var plane1 = new me.feng3d.Object3D("plane1", [
-        me.feng3d.primitives.createPlane(),
-        new me.feng3d.Space3D(-100, -100, 300, 90, 0, 45),
-    ]);
-    view3D.scene.addObject3D(plane1);
+            var plane = new me.feng3d.Object3D("plane", [
+                me.feng3d.primitives.createPlane(),
+                new me.feng3d.Space3D(100, 100, 300, 90, 0, 45),
+            ]);
+            this.view3D.scene.addObject3D(plane);
 
-    var sphere = new me.feng3d.Object3D("sphere", [
-        me.feng3d.primitives.createSphere(),
-        new me.feng3d.Space3D(0, -100, 300, 90, 0, 45),
-    ]);
-    view3D.scene.addObject3D(sphere);
+            var sphere = new me.feng3d.Object3D("sphere", [
+                me.feng3d.primitives.createSphere(),
+                new me.feng3d.Space3D(0, -100, 300, 90, 0, 45),
+            ]);
+            this.view3D.scene.addObject3D(sphere);
 
-    var capsule = new me.feng3d.Object3D("capsule", [
-        me.feng3d.primitives.createCapsule(),
-        new me.feng3d.Space3D(200, -50, 500, 0, 0, 0),
-    ]);
-    view3D.scene.addObject3D(capsule);
+            var capsule = new me.feng3d.Object3D("capsule", [
+                me.feng3d.primitives.createCapsule(),
+                new me.feng3d.Space3D(200, -50, 500, 0, 0, 0),
+            ]);
+            this.view3D.scene.addObject3D(capsule);
 
-    var cylinder = new me.feng3d.Object3D("cylinder", [
-        me.feng3d.primitives.createCylinder(),
-        new me.feng3d.Space3D(200, -200, 500, -90, 0, 0),
-    ]);
-    view3D.scene.addObject3D(cylinder);
+            var cylinder = new me.feng3d.Object3D("cylinder", [
+                me.feng3d.primitives.createCylinder(),
+                new me.feng3d.Space3D(200, -200, 500, -90, 0, 0),
+            ]);
+            this.view3D.scene.addObject3D(cylinder);
+        }
+
+    }
+
 }
 
-var view3D: me.feng3d.View3D;
-
-start();
+new me.feng3d.WebglDemo();
